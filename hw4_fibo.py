7587fb51215c4f2dce2b067eb2582e366fec58ab
106071023 <37078444+106071023@users.noreply.github.com>
2018-04-17 20:00:51 +0800

    Update hw4_fibo.py

--- hw4_fibo.py
+++ hw4_fibo.py
@@ -2,8 +2,12 @@
 
 
 def compute_fibonacci(num):
-    # your code here ...
-    return ...
+    if num == 0:
+        return 0
+    elif num == 1:
+        return 1
+    else:
+        return compute_fibonacci(num-1)+compute_fibonacci(num-2)
 
 
 num = int(input("你想看費氏數列第幾個數字？ "))
