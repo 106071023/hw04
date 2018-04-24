2b0e6bdc093fc6b386920321051fb068fd24cb9b
106071023 <37078444+106071023@users.noreply.github.com>
2018-04-24 16:55:01 +0800

    Update hw4_prime.py

--- hw4_prime.py
+++ hw4_prime.py
@@ -2,12 +2,13 @@
 
 
 def is_prime(num):
-    if num == 1:
+    if num==1:
         return False
     for i in range(2,int(num**(1/2)+1)):
-        if num%i == 0:
+        if num%i==0:
             return False
     return True
+
 
 
 num = int(input("Enter a number: "))
